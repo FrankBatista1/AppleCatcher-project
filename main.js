7a5a1325261720d1e814e8f998f86bd399c423d4
FrankBatista1 <fb53901@gmail.com>
2021-08-23 11:51:40 -0400

    Player movement

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-canvas.height = 800
-canvas.widht = 800
+canvas.height = 400
+canvas.widht = 400  
 
 //tracks keys the player is pressing
 const keys = []
 
 const player = {
-  x: 0,
-  y: 0,
-  widht: 63,
-  height: 63,
+  x: 100,
+  y: 250,
+  widht: 32,
+  height: 48,
   frameX: 0,
   frameY: 0,
-  speed: 9,  //fps
+  speed: 2,  //fps
   moving: false
 };
 
@@ -24,15 +24,73 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 // hight of spritesheet 63
 // widht of spritesheet 63
 const playerSprite = new Image();
-playerSprite.src = './Images/Rhinoceros sprite sheet needed _ OpenGameArt.org_files/behomath.png'
+playerSprite.src = './Images/gau.png'
 
 const background = new Image()
-background.src = './Images/Hd-2048x2048-A-Visitors-Guide-to-Dantes-Nine-Circles-of-Hell.jpeg'
+background.src = './Images/ce4f9092-606e-48d6-a6a8-cad7e0b6e21d.jpeg' 
 
-function animate(){
-  ctx.clearRect(0,0,canvas.widht, canvas.height)
-  ctx.drawImage(background,0,0,canvas.widht,canvas.height)
-  drawSprite(playerSprite, 0, 0, player.widht, player.height, 200, 300, player.widht, player.height)
-  requestAnimationFrame(animate);
+window.addEventListener('keydown',function(e){
+    keys[e.key] = true;
+    player.moving = true;
+})
+
+window.addEventListener('keyup', (e) => {
+   delete keys[e.key];
+   player.moving = false
+})
+
+function movePlayer(){
+  if(keys['ArrowUp'] && player.y > 210){
+    player.y -= player.speed;
+    player.frameY = 3;
+    player.moving = true;
+  }
+  if (keys['ArrowDown'] && player.y < 350){
+    player.y += player.speed;
+    player.frameY = 0;
+    player.moving = true;
+  }
+  if (keys['ArrowLeft'] && player.x > -3){
+    player.x -= player.speed;
+    player.frameY = 1;
+    player.moving = true;
+  }
+  if (keys['ArrowRight'] &&  player.x < 400){
+    player.x += player.speed;
+    player.frameY = 2;
+    player.moving = true;
+  }
 }
-animate();
+
+function walkingAnimation(){
+  if(player.frameX < 3 && player.moving === true){
+    player.frameX++;
+  } else{
+    player.frameX = 0;
+  }
+}
+
+let fps, fpsInterval, startTime, now, then, elapsed;
+
+function StartAnimating(fps){
+  fpsInterval = 1000/fps
+  then = Date.now()
+  startTime = then;
+  animate();
+}
+function animate(){
+  requestAnimationFrame(animate)
+  now = Date.now()
+  elapsed = now - then;
+  if (elapsed > fpsInterval){
+    then = now - (elapsed % fpsInterval)
+    ctx.clearRect(0,0,canvas.widht, canvas.height)
+    ctx.drawImage(background,0,0,canvas.widht,canvas.height)
+    drawSprite(playerSprite, player.widht * player.frameX, player.height * player.frameY, player.widht, player.height, player.x, player.y, player.widht, player.height)
+    requestAnimationFrame(animate);
+    movePlayer()
+    walkingAnimation()
+  }
+ 
+}
+StartAnimating(30)
