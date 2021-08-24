9c8c23e25433933ea32d4a8d1dbae77058e3f8ca
FrankBatista1 <fb53901@gmail.com>
2021-08-24 14:47:59 -0400

    Display apples with classes

--- main.js
+++ main.js
@@ -15,78 +15,19 @@ const player = {
   height: 48,
   frameX: 0,
   frameY: 0,
-  speed: 7,  //fps
+  speed: 9,  
   moving: false
 };
 
-const numberOfApples = 10
-const apples = [];
 
-class Apples{
-  constructor(){
-    this.x = 130,
-    this.y = -400,
-    this.width = 32,
-    this.height = 32
-    this.speed = 5
-  }
-  
-  appleRandomized(){
-    this.x = Math.floor(Math.random() * 250)
-  }
-}
-const apple = new Apples
-const apple2 = new Apples
-const apple3 = new Apples
-const apple4 = new Apples
-const apple5 = new Apples
-const apple6 = new Apples
-const apple7 = new Apples
-const apple8 = new Apples
-const apple9 = new Apples
-const apple10 = new Apples
-apple.appleRandomized()
-apple2.appleRandomized()
-apple3.appleRandomized()
-apple4.appleRandomized()
-apple5.appleRandomized()
-apple6.appleRandomized()
-apple7.appleRandomized()
-apple8.appleRandomized()
-apple9.appleRandomized()
-apple10.appleRandomized()
-
-function appleFalling(){
-  apple10.y += 2.3
-  apple9.y += 4.1
-  apple8.y += 3.9
-  apple7.y += 2.5
-  apple6.y += 2.1
-  apple5.y += 1.8
-  apple4.y += 3
-  apple3.y += 1.9
-  apple2.y += 3.2
-  apple.y += 4
-}
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
   ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH)
 }
 
-// function drawApples(img, sX, sY,count){
-//   count++;
-//   if(count % 40 == 0){
-//   ctx.drawImage(img, sX, sY)
-//     console.log('yei')
-//   } else{
-//     console.log('hey')
-//   }
-// }
 
 
 
-// hight of spritesheet 63
-// widht of spritesheet 63
 const playerSprite = new Image();
 playerSprite.src = './Images/gau.png'
 
@@ -95,21 +36,27 @@ applesImg.src = './Images/apple.png'
 
 const background = new Image()
 background.src = './Images/ce4f9092-606e-48d6-a6a8-cad7e0b6e21d.jpeg' 
+const numberOfApples = 15
+const apples = [];
 
-// let counter = 0;
-// function drawTheApple(){
-//     let randomNumber = (Math.floor(Math.random * 200))
-//     ctx.drawImage(applesImg, -10,randomNumber)
-//     return counter++;
-// }
-// const drawApples = setInterval(drawTheApple,6000)
-// function stopInterval() {
-//   if(counter === 20){
-//     clearInterval(drawApples)
-//     }
-//   } 
-// drawTheApple()
-// stopInterval()
+class Apples{
+  constructor(){
+    this.x = Math.floor(Math.random() * 250),
+    this.y = -400,
+    this.width = 32,
+    this.height = 32
+    this.speed = (Math.random() * 4.9) + 3
+  }
+    draw(){
+      ctx.drawImage(applesImg,this.x,this.y)
+    }
+    fall(){
+      this.y += this.speed
+    }
+  }
+for (let i = 0; i < numberOfApples; i++){
+  apples.push(new Apples())
+}
 
 
 
@@ -174,18 +121,11 @@ function animate(){
     ctx.clearRect(0,0,canvas.widht, canvas.height)
     ctx.drawImage(background,0,0,canvas.widht,canvas.height)
     drawSprite(playerSprite, player.widht * player.frameX, player.height * player.frameY, player.widht, player.height, player.x, player.y, player.widht, player.height)
-    ctx.drawImage(applesImg,apple.x,apple.y)
-    ctx.drawImage(applesImg,apple2.x,apple2.y)
-    ctx.drawImage(applesImg,apple3.x,apple3.y)
-    ctx.drawImage(applesImg,apple4.x,apple4.y)
-    ctx.drawImage(applesImg,apple5.x,apple5.y)
-    ctx.drawImage(applesImg,apple6.x,apple6.y)
-    ctx.drawImage(applesImg,apple7.x,apple7.y)
-    ctx.drawImage(applesImg,apple8.x,apple8.y)
-    ctx.drawImage(applesImg,apple9.x,apple9.y)
-    ctx.drawImage(applesImg,apple10.x,apple10.y)
+    for (let i = 0; i < apples.length; i++){
+      apples[i].draw()
+      apples[i].fall()
+    }
   
-    appleFalling()
     movePlayer()
     // console.log((Number(then.toFixed(0))))
     walkingAnimation()
