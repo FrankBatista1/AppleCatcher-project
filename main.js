3555df1fc7dd74c9c32ecd980ec4005a4cb5cddb
FrankBatista1 <fb53901@gmail.com>
2021-08-26 14:50:25 -0400

    Final game no sound

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 
+
+let apples = [];
+let applesCatched = [];
+let keys = [];
+let timesLost = 0
+let timesWon = 0
+
 function start(numberOfApples) {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   canvas.height = 400;
   canvas.widht = 400;
-  let value = window.devicePixelRatio;
-  value = 2
+  
 
   //tracks keys the player is pressing
-  let keys = [];
+  
 
   let player = {
     x: 130,
@@ -31,8 +37,7 @@ function start(numberOfApples) {
 
   const background = new Image();
   background.src = "./Images/ce4f9092-606e-48d6-a6a8-cad7e0b6e21d.jpeg";
-  let apples = [];
-  let applesCatched = [];
+  
 
   class Apples {
     constructor() {
@@ -97,8 +102,7 @@ function start(numberOfApples) {
   }
 
   let fpsInterval, now, then, elapsed;
-  let timesLost = 0
-  let timesWon = 0
+  
  
 
   function getDistance(x1, y1, x2, y2) {
@@ -193,14 +197,59 @@ function startGame(numberOfApples) {
 }
 
 function reset(){
+  apples = [];
+  applesCatched = [];
+  keys = [];
+  timesLost = 0;
+  timesWon = 0;
+
   let startDiv = document.getElementById("start");
   let gameCanvas = document.getElementById("canvas");
   let resetGame = document.getElementById("reset");
   let chooseDifficulty = document.getElementById("choose-difficulty")
+  
   startDiv.style.display = "none";
   gameCanvas.style.display = "none";
   resetGame.style.display = "none"
   chooseDifficulty.style.display ="block"
+
+  
+}
+
+function rerenderApples(numberOfApplesRerendered){
+  let startDiv = document.getElementById("start");
+  let gameCanvas = document.getElementById("canvas");
+  let resetGame = document.getElementById("reset");
+  let chooseDifficulty = document.getElementById("choose-difficulty")
+  const ctx = gameCanvas.getContext("2d");
+  
+  startDiv.style.display = "none";
+  gameCanvas.style.display = "block";
+  resetGame.style.display = "block"
+  chooseDifficulty.style.display ="none"
+  
+  const applesImg = new Image();
+  applesImg.src = "./Images/apple.png";
+
+  class Apples {
+    constructor() {
+      (this.x = Math.floor(Math.random() * 250)),
+        (this.y = -400),
+        (this.width = 32),
+        (this.height = 32);
+      this.speed = Math.random() * 3.1 + 1.8;
+    }
+    draw() {
+      ctx.drawImage(applesImg, this.x, this.y);
+    }
+    fall() {
+      this.y += this.speed;
+    }
+    pushOut() {}
+  }
+  for (let i = 0; i < numberOfApplesRerendered; i++) {
+    apples.push(new Apples());
+  }
 }
 
 
